bcba62937660c3db0c9f21d2bcab93ec8aa5758b
Quy <duanhquy@gmail.com>
2017-02-10 09:39:15 +0700

    1

--- app/menu.component.js
+++ app/menu.component.js
@@ -8,14 +8,16 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var MenuClassComponent = (function () {
     function MenuClassComponent() {
+        this.applyClass = true;
+        this.blueColor = false;
     }
     return MenuClassComponent;
 }());
 MenuClassComponent = __decorate([
     core_1.Component({
         selector: 'my-menu',
-        template: "<ul>\n    <li>Du Anh Quy<li>\n    <li>FPT</li>\n    <li>UTEHY</li>\n    </ul>\n    <h4>X1jfslf</h4>\n    ",
-        styles: ['h4 {color:red;}']
+        template: "\n    <h1>This is my menu</h1>\n    <h3 [class.redColor]=\"applyClass\">Apply Class</h3>\n    <h4 [style.color]=\"blueColor?'blue':'orange'\">Apply Style</h4>\n    ",
+        styles: [".redColor{\n            color:red;\n        }"]
     })
 ], MenuClassComponent);
 exports.MenuClassComponent = MenuClassComponent;
--- app/menu.component.ts
+++ app/menu.component.ts
@@ -2,16 +2,18 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'my-menu',
-    template:`<ul>
-    <li>Du Anh Quy<li>
-    <li>FPT</li>
-    <li>UTEHY</li>
-    </ul>
-    <h4>X1jfslf</h4>
+    template:`
+    <h1>This is my menu</h1>
+    <h3 [class.redColor]="applyClass">Apply Class</h3>
+    <h4 [style.color]="blueColor?'blue':'orange'">Apply Style</h4>
     `,
-    styles:['h4 {color:red;}']
+    styles:[`.redColor{
+            color:red;
+        }`]
 })
 
 export class MenuClassComponent{
+    public applyClass = true;
+    public blueColor = false;
 
 }
